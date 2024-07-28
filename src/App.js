be3004b3684f1df20e4d6a92a1486265a08a451d
Kevin Cripe <kevincripe6@gmail.com>
2024-07-28 12:50:29 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const title = 'Welcome to the new Blog';
   const likes = 50;
 
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
+      </Routes>
       </div>
     </div>
+    </Router>
   );
 }
 
