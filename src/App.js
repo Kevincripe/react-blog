f54d1071b641585b87d05cc385465febe5751989
Kevin Cripe <kevincripe6@gmail.com>
2024-07-28 13:20:13 -0500

    create link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   const title = 'Welcome to the new Blog';
@@ -11,12 +12,12 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-
-      </Routes>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
